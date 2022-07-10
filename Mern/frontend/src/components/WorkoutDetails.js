a40d399277d671dfe3e5207d14dc063efe3617a5
Thanat <bztzn512@gmail.com>
2022-07-09 22:42:32 -0400

    deleteItem

--- Mern/frontend/src/components/WorkoutDetails.js
+++ Mern/frontend/src/components/WorkoutDetails.js
@@ -1,7 +1,21 @@
 import React from "react";
+import { useWorkoutContext } from "../hooks/useWorkoutContext";
 
 const WorkoutDetails = ({ workout }) => {
   const { title, reps, load, createdAt } = workout;
+  const { dispatch } = useWorkoutContext();
+
+  const clickHandler = async () => {
+    const response = await fetch("/api/workouts/" + workout._id, {
+      method: "DELETE",
+    });
+    const json = await response.json();
+
+    if (response.ok) {
+      dispatch({ type: "DELETE_WORKOUT", payload: json });
+    }
+  };
+
   return (
     <div className="workout-details">
       <h4>{title}</h4>
@@ -14,6 +28,7 @@ const WorkoutDetails = ({ workout }) => {
         {reps}
       </p>
       <p>{createdAt}</p>
+      <span onClick={clickHandler}>delete</span>
     </div>
   );
 };
