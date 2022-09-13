c16061b4d04e1b70e491649e8654b458f3a844f0
Thanat <bztzn512@gmail.com>
2022-09-12 20:31:18 -0400

    typeScript

---
+++ typeScript/sandbox.js
@@ -0,0 +1,17 @@
+var names = ["apple", "pineapple", "orange"];
+var char;
+var age;
+var loggin;
+/*
+let info = {
+  char: "jim",
+  age: 34,
+  loggin: false,
+}; */
+char = "mike";
+age = 23;
+loggin = true;
+//array
+var fruits = [];
+fruits.push("apple", "orange", 20);
+console.log(fruits);
---
+++ typeScript/sandbox.ts
@@ -0,0 +1,21 @@
+let names = ["apple", "pineapple", "orange"];
+
+let char: string;
+let age: number;
+let loggin: boolean;
+/* 
+let info = {
+  char: "jim",
+  age: 34,
+  loggin: false,
+}; */
+
+char = "mike";
+age = 23;
+loggin = true;
+
+//array
+let fruits: (string | number)[] = [];
+fruits.push("apple", "orange", 20);
+
+console.log(fruits);
