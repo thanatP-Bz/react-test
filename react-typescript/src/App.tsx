b44a7fa44da242bb3bcc30c4e504fc3c35f2b41a
Thanat <bztzn512@gmail.com>
2022-09-17 22:08:26 -0400

    typeScript

--- react-typescript/src/App.tsx
+++ react-typescript/src/App.tsx
@@ -8,6 +8,12 @@ const App: React.FC = () => {
 
   const handlerSubmit = (e: React.FormEvent) => {
     e.preventDefault();
+
+    if (todo) {
+      setTodos([...todos, { id: Date.now(), todo, isDone: false }]);
+      setTodo("");
+    }
+    console.log(todos);
   };
 
   return (
--- react-typescript/src/models/model.ts
+++ react-typescript/src/models/model.ts
@@ -1,5 +1,5 @@
 export interface Todo {
   id: number;
   todo: string;
-  isDrop: boolean;
+  isDone: boolean;
 }
