756a73c8737e2caa2c3ad9ade25c3972f00f8ff9
Thanat <bztzn512@gmail.com>
2022-10-04 22:20:38 -0400

    redux-toolkit

---
+++ react-typescript/src/models/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import CheckUser from "./components/CheckUser";
+import List from "./components/List";
+
+const App = () => {
+  return (
+    <div>
+      <List />
+    </div>
+  );
+};
+
+export default App;
