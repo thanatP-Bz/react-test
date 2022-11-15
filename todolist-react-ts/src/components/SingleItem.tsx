23aef592c75100960f1df9d8bf0c9014e8e33dd7
Thanat <bztzn512@gmail.com>
2022-11-14 20:14:16 -0500

    fix passing props

--- todolist-react-ts/src/components/SingleItem.tsx
+++ todolist-react-ts/src/components/SingleItem.tsx
@@ -1,14 +1,32 @@
 import React from "react";
 import { Todo } from "../components/Todo";
+import { AiOutlineCheck } from "react-icons/ai";
+import { BsTrashFill } from "react-icons/bs";
 
-interface Props {
+type Props = {
   todo: Todo;
   item: Todo[];
   setItem: React.Dispatch<React.SetStateAction<Todo[]>>;
-}
+};
 
-const SingleItem: React.FC<Props> = ({ todo, item, setItem }) => {
-  return <div>SingleItem</div>;
+const SingleItem = ({ todo, item, setItem }: Props) => {
+  return (
+    <form className="bg-white w-full text-xl text-gray-700 shadow-lg rounded-lg px-8 py-3 md:p-5 mb-3 md:mb-6 flex justify-between">
+      <div>
+        <ul>
+          <li>{todo.todo}</li>
+        </ul>
+      </div>
+      <div className="flex">
+        <span className="p-2 rounded-md text-blue-400 cursor-pointer">
+          <AiOutlineCheck />
+        </span>
+        <span className="p-2 rounded-md text-blue-400 cursor-pointer">
+          <BsTrashFill />
+        </span>
+      </div>
+    </form>
+  );
 };
 
 export default SingleItem;
