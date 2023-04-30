9274eeef9ea1a3264507447b5d1edac835bba8ec
Thanat <bztzn512@gmail.com>
2023-04-30 12:26:09 -0400

    fixedReducer

--- expenses-tracker-ts/src/contexts/UseContext.tsx
+++ expenses-tracker-ts/src/contexts/UseContext.tsx
@@ -2,7 +2,7 @@ import { createContext } from "react";
 
 export interface Expense {
   _id: number;
-  text?: string;
+  text: string;
   amount: number;
 }
 
@@ -10,8 +10,33 @@ type initialStateType = {
   transactions: Expense[];
 };
 
+type Action =
+  | { type: "DELETE_TRANSACTION"; payload: number }
+  | { type: "ADD_TRANSACTION"; payload: string };
+
 export const initialState: initialStateType = {
-  transactions: [],
+  transactions: [{ _id: 1, text: "glass", amount: 34 }],
 };
+
+const reducer = (state: typeof initialState, action: Action) => {
+  switch (action.type) {
+    case "DELETE_TRANSACTION":
+      return {
+        ...state,
+        transactions: state.transactions.filter(
+          (transaction) => transaction._id !== action.payload
+        ),
+      };
+    case "ADD_TRANSACTION":
+      return {
+        ...state,
+        transactions: [...state.transactions, { text: action.payload }],
+      };
+    default:
+      return state;
+  }
+};
+
+export default reducer;
 
 const AppContext = createContext(initialState);
--- expenses-tracker-ts/src/contexts/reducer.ts
+++ expenses-tracker-ts/src/contexts/reducer.ts
@@ -1,27 +1,14 @@
-import { Actions } from "./action";
+/* import { Actions } from "./action";
 import { initialState } from "./UseContext";
 
-const reducer = (
-  state: typeof initialState,
-  action: Actions
-): typeof initialState => {
-  switch (action.type) {
-    case "DELETE_TRANSACTION":
-      return {
-        ...state,
-        transactions: state.transactions.filter(
-          (transaction) => transaction._id !== action.payload
-        ),
-      };
-    case "ADD_TRANSACTION":
-      return {
-        ...state,
-        transactions: [],
-      };
+const reducer = (state, action) => {
+  switch(action.type) {
 
     default:
       return state;
   }
-};
 
-export default reducer;
+  }
+
+
+export default reducer; */
