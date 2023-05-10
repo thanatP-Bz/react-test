e543775e3488b4b0937700ee41d5bd52eab55855
Thanat <bztzn512@gmail.com>
2023-05-10 15:43:02 -0400

    .

--- expenses-tracker-ts/backend/model/expenseModel.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-const { Schema } = mongoose;
-
-const Expense = new Schema({
-  text: String,
-  amount: Number,
-});
-
-export default mongoose.model("Expense", Expense);
