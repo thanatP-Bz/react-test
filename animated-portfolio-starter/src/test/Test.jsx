cb8575a74d9525103820c9f29c208ff0d210f918
Thanat <bztzn512@gmail.com>
2024-05-04 22:50:10 -0400

    framerMotionPlayAround

--- animated-portfolio-starter/src/test/Test.jsx
+++ animated-portfolio-starter/src/test/Test.jsx
@@ -1,10 +1,28 @@
+import "./test.scss";
 import { motion } from "framer-motion";
 
 const Test = () => {
+  const variants = {
+    visible: { opacity: 1 },
+    hidden: { opacity: 0 },
+  };
   return (
-    <motion.div className="course">
-      <div className="box"></div>
-    </motion.div>
+    <div className="course">
+      <motion.div
+        initial="hidden"
+        animate="visible"
+        variants={variants}
+        transition={{ ease: "easeIn", duration: 2 }}
+        className="box"
+      ></motion.div>
+      <motion.div
+        initial="hidden"
+        animate="visible"
+        variants={variants}
+        transition={{ ease: "easeIn", duration: 4 }}
+        className="box-1"
+      ></motion.div>
+    </div>
   );
 };
 
