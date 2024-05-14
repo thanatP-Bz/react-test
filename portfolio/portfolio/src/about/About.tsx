596ebddfdbf3e0d48f4c73fad02ee47155857bf0
Thanat <bztzn512@gmail.com>
2024-05-13 21:35:12 -0400

    addAboutSection

--- portfolio/portfolio/src/about/About.tsx
+++ portfolio/portfolio/src/about/About.tsx
@@ -8,14 +8,22 @@ const About = () => {
         <img src={img2} alt="beach" />
       </div>
 
-      <div className="content-container">
+      <div className="about-content-container">
         <h1>ABOUT ME</h1>
-        <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quaerat
-          sequi dolorum consequatur a totam maxime nam possimus perspiciatis
-          harum explicabo alias saepe laboriosam, nobis aliquam, inventore
-          dolorem, qui corporis expedita.
-        </p>
+        <div className="text-wrapper">
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quaerat
+            sequi dolorum consequatur a totam maxime nam possimus perspiciatis
+            harum explicabo alias saepe laboriosam, nobis aliquam, inventore
+            dolorem, qui corporis expedita.
+          </p>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quaerat
+            sequi dolorum consequatur a totam maxime nam possimus perspiciatis
+            harum explicabo alias saepe laboriosam, nobis aliquam, inventore
+            dolorem, qui corporis expedita.
+          </p>
+        </div>
       </div>
     </div>
   );
